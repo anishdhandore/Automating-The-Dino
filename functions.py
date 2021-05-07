cbd79cca6fe206d2f1fceba7a8491bba5c978292
Anish <anish.dhandore@gmail.com>
2021-05-07 23:35:09 +0530

    created functions

---
+++ functions.py
@@ -0,0 +1,57 @@
+import pyautogui
+from PIL import Image, ImageGrab
+import time
+
+def hit(key):
+    pyautogui.keyDown(key)
+    return 
+
+# run when the screen is white
+def CollisionWhiteScreen(data):
+     # collision with the birds
+    for i in range(550,650):
+        for j in range(500,563):
+            # if value of color less than 100 then consider it black!
+            if (data[i,j]) < 100: 
+                hit("down")
+                time.sleep(0.5)
+                pyautogui.keyUp("down")
+                return
+ 
+    # collision with the cactus
+    for i in range(550,650):
+        for j in range(563,650):
+            # if value of color less than 100 then consider it black!
+            if (data[i,j]) < 100:  
+                hit("up")
+                return
+    return
+
+# run when the screen turns dim
+def CollisionBlackScreen(data):
+    # collision with the birds
+    for i in range(550,650):
+        for j in range(500,563):
+            # if value of color greater than 100 then consider it white!
+            if (data[i,j]) > 100:  
+                hit("down")
+                time.sleep(0.5)
+                pyautogui.keyUp("down")
+                return
+ 
+    # collision with the cactus
+    for i in range(550,650):
+        for j in range(563,650):
+            # if value of color greater than 100 then consider it white!
+            if (data[i,j]) > 100:  
+                hit("up")
+                return
+    return
+
+def CheckScreenColor(data):
+    for i in range(700,900):
+        for j in range(150,300):
+            if data[i,j] < 100:
+                # return true if screen is black/dim
+                return True
+    return False
